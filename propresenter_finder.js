a121245ad7de991ad064b3516e559c58c7e02f1f
Johan Swetzén <johan@swetzen.com>
2015-07-12 10:05:28 +0200

    Modify mDNS resolver sequence to stop Raspberry Pi errors.

--- propresenter_finder.js
+++ propresenter_finder.js
@@ -5,7 +5,12 @@ var mdns = require('mdns');
 var net = require('net');
 
 var BonjourFinder = function(serviceName, onFind) {
-  var browser = mdns.createBrowser(mdns.tcp(serviceName));
+  var sequence = [
+    mdns.rst.DNSServiceResolve(),
+    'DNSServiceGetAddrInfo' in mdns.dns_sd ? mdns.rst.DNSServiceGetAddrInfo() : mdns.rst.getaddrinfo({families:[4]}),
+    mdns.rst.makeAddressesUnique()
+  ];
+  var browser = mdns.createBrowser(mdns.tcp(serviceName), {resolverSequence: sequence});
   var callback = onFind;
 
   var serviceUp = function(service) {
