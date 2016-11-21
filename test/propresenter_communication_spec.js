ccb7dc9ac74d48c31af448b1935df9992f9ff54c
Johan Swetzén <johan.swetzen@cybercom.com>
2016-11-21 21:31:44 +0100

    Fixes #14 countdown not working on ProPresenter 6

--- test/propresenter_communication_spec.js
+++ test/propresenter_communication_spec.js
@@ -10,19 +10,28 @@ var parseTime = ppcommunication.__get__('parseTime');
 describe("ProPresenter Communication", function() {
   describe("#parseTime()", function() {
     it("should parse Mac AM times", function() {
-      expect(parseTime("9:21:23 AM")).to.equal("09:21:23");
+      expect(parseTime("9:21:23 AM")).to.equal(9*3600 + 21*60 + 23);
     });
     it("should parse Mac PM times", function() {
-      expect(parseTime("9:21:23 PM")).to.equal("21:21:23");
+      expect(parseTime("9:21:23 PM")).to.equal(21*3600 + 21*60 + 23);
     });
     it("should parse Windows times", function() {
-      expect(parseTime("09:21 ")).to.equal("09:21:00");
+      expect(parseTime("09:21 ")).to.equal(9*3600 + 21*60 + 0);
     });
     it("should parse Windows AM times", function() {
-      expect(parseTime("09:21 am")).to.equal("09:21:00");
+      expect(parseTime("09:21 am")).to.equal(9*3600 + 21*60 + 0);
     });
     it("should parse Windows PM times", function() {
-      expect(parseTime("09:21 pm")).to.equal("21:21:00");
+      expect(parseTime("09:21 pm")).to.equal(21*3600 + 21*60 + 0);
+    });
+    it("should parse countdown", function() {
+      expect(parseTime("00:21:03")).to.equal(0*3600 + 21*60 + 3);
+    });
+    it("should parse pro6 countdown", function() {
+      expect(parseTime("0:21:03")).to.equal(0*3600 + 21*60 + 3);
+    });
+    it("should parse pro6 countdown with hour", function() {
+      expect(parseTime("1:01:03")).to.equal(1*3600 + 1*60 + 3);
     });
   });
 });
