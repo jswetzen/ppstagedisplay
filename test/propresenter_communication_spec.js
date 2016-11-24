ab74600e2e26c5db3448752a6cc8dfed19b0bb0f
Johan Swetzén <johan@swetzen.com>
2016-11-24 16:16:59 +0100

    Update tests

--- test/propresenter_communication_spec.js
+++ test/propresenter_communication_spec.js
@@ -9,35 +9,26 @@ var parseTime = ppcommunication.__get__('parseTime');
 
 describe("ProPresenter Communication", function() {
   describe("#parseTime()", function() {
-    it("should parse Mac AM times", function() {
+    it("should parse Mac time", function() {
       expect(parseTime("9:21:23 AM")).to.equal(9*3600 + 21*60 + 23);
-    });
-    it("should parse Mac PM times", function() {
       expect(parseTime("9:21:23 PM")).to.equal(21*3600 + 21*60 + 23);
     });
-    it("should parse Windows times", function() {
+    it("should parse Windows time", function() {
       expect(parseTime("09:21 ")).to.equal(9*3600 + 21*60 + 0);
-    });
-    it("should parse Windows AM times", function() {
       expect(parseTime("09:21 am")).to.equal(9*3600 + 21*60 + 0);
-    });
-    it("should parse Windows PM times", function() {
       expect(parseTime("09:21 pm")).to.equal(21*3600 + 21*60 + 0);
     });
     it("should parse countdown", function() {
       expect(parseTime("00:21:03")).to.equal(0*3600 + 21*60 + 3);
+      expect(parseTime("-00:21:03")).to.equal(-1*(0*3600 + 21*60 + 3));
+      expect(parseTime("00:00:00")).to.equal(0);
     });
     it("should parse pro6 countdown", function() {
       expect(parseTime("0:21:03")).to.equal(0*3600 + 21*60 + 3);
-    });
-    it("should parse pro6 countdown with hour", function() {
       expect(parseTime("1:01:03")).to.equal(1*3600 + 1*60 + 3);
-    });
-    it("should parse pro6 negative countdown", function() {
       expect(parseTime("-0:21:03")).to.equal(-1*(0*3600 + 21*60 + 3));
-    });
-    it("should parse pro6 negative countdown with hour", function() {
       expect(parseTime("-1:01:03")).to.equal(-1*(1*3600 + 1*60 + 3));
+      expect(parseTime("0:00:00")).to.equal(0);
     });
   });
 });
