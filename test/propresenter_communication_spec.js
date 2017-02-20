3f15f9d6d6223cfbca499aff7ec0b96d0acfb740
Kevin Doveton <kevindoveton@users.noreply.github.com>
2017-02-21 02:15:31 +1030

    Improve and test parseColors function (#18)
    * updated parseColors to handle errors better
    
    * updated tests to include parseColors

--- test/propresenter_communication_spec.js
+++ test/propresenter_communication_spec.js
@@ -6,8 +6,10 @@ var expect = require("chai").expect;
 var ppcommunication = rewire("../lib/propresenter_communication.js");
 
 var parseTime = ppcommunication.__get__('parseTime');
+var parseColors = ppcommunication.__get__('parseColors');
 
 describe("ProPresenter Communication", function() {
+	
   describe("#parseTime()", function() {
     it("should parse Mac time", function() {
       expect(parseTime("9:21:23 AM")).to.equal(9*3600 + 21*60 + 23);
@@ -33,6 +35,55 @@ describe("ProPresenter Communication", function() {
     it("should handle non-parsable times", function() {
       expect(parseTime("--:--:--")).to.equal(0);
     });
+  });
+  
+  describe("#parseColors()", function() {
+		  it("should parse red", function() {
+			  var input = {red:255,green:0,blue:0,alpha:0}
+			  var expected = input;
+			  expected.htmlColor = 'rgba(255,0,0,0)'
+			  expect(parseColors(input)).to.equal(expected);
+		  })
+		  
+		  it("should parse green", function() {
+			var input = {red:0,green:255,blue:0,alpha:0}
+		  	var expected = input;
+		  	expected.htmlColor = 'rgba(0,255,0,0)';
+		  	expect(parseColors(input)).to.equal(expected);
+	  	  })
+		  
+		  it("should parse blue", function() {
+			var input = {red:0,green:0,blue:255,alpha:0}
+			var expected = input;
+			expected.htmlColor = 'rgba(0,0,255,0)';
+			expect(parseColors(input)).to.equal(expected);
+		  })
+		  
+		  it("should parse alpha", function() {
+			var input = {red:0,green:0,blue:0,alpha:255}
+			var expected = input;
+			expected.htmlColor = 'rgba(0,0,0,255)';
+			expect(parseColors(input)).to.equal(expected);
+		  })
+		  
+		  it("should parse a combination", function() {
+			var input = {red:54,green:73,blue:20,alpha:123}
+			var expected = input;
+			expected.htmlColor = 'rgba(54,73,20,123)';
+			expect(parseColors(input)).to.equal(expected);
+		  })
+		  
+		  it("should parse null as black", function() {
+			  var input = null;
+			  var expected = {red: 0, green: 0, blue: 0, alpha: 1, htmlColor: 'rgba(0,0,0,255)'};
+			  expect(parseColors(input)).to.deep.equal(expected);
+		  })
+		  
+		  it("should parse undefined as black", function() {
+			  var input = undefined;
+			  var expected = {red: 0, green: 0, blue: 0, alpha: 1, htmlColor: 'rgba(0,0,0,255)'};
+			  expect(parseColors(input)).to.deep.equal(expected);
+		  })
   });
 });
 
